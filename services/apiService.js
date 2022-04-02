a2322daa2a1048eaba44d07a652fe3f7d54f03df
Codalien Technologies <codalientechnologies@Codaliens-MacBook-Air.local>
2022-04-02 15:40:29 +0530

    Developed : Base setup

---
+++ services/apiService.js
@@ -0,0 +1,10 @@
+const axios = require('axios')
+const dotenv = require('dotenv')
+dotenv.config()
+
+const apiKey = process.env.YOUTUBE_KEY
+const baseUri = process.env.YOUTUBE_BASE
+
+const getSearchYT = async () =>{
+
+}
